95944522b0cb946001fb2610f1273d8189518e43
Eli <88557639+lishaduck@users.noreply.github.com>
2024-11-08 13:19:02 -0600

    feat: do "not quite lisp"

--- src/aoc/__main__.py
+++ src/aoc/__main__.py
@@ -20,8 +20,10 @@ p: Path = Path(__file__).resolve().parent
 est = datetime.timezone(datetime.timedelta(hours=-4))
 now = datetime.datetime.now(tz=est)
 
-active_year = now.year if now.month == 12 else now.year - 1
-active_day = 1
+is_december = now.month == 12
+is_christmas = is_december and now.day == 25
+active_year = now.year if is_december else now.year - 1
+active_day = now.day if is_december else 1
 
 
 @app.command()
@@ -32,7 +34,7 @@ def scaffold(
     ] = active_day,
     year: Annotated[
         int,
-        typer.Option(show_default=f"{now:%Y}"),
+        typer.Option(show_default=f"{now:%Y}", prompt=""),
     ] = active_year,
 ) -> None:
     year_folder: Path = p / f"Y{year}"
@@ -40,6 +42,8 @@ def scaffold(
     in_file: Path = day_folder / f"{day:0>2}.in"
     out_file_a: Path = day_folder / f"{day:0>2}a.out"
     out_file_b: Path = day_folder / f"{day:0>2}b.out"
+    main_file: Path = day_folder / "main.py"
+    test_file: Path = day_folder / f"test_{day:0>2}.py"
     init_year_file: Path = year_folder / "__init__.py"
     init_day_file: Path = day_folder / "__init__.py"
 
@@ -55,6 +59,29 @@ def scaffold(
         progress.add_task(description="Writing data...", total=None)
 
         day_folder.mkdir(parents=True, exist_ok=True)
+
+        if not main_file.exists():
+            main_file.write_text(f"""from typing import override
+
+from aoc.base import BaseSolution
+
+
+class Solution(BaseSolution[str]):
+    @override
+    def parse(self, values: str) -> str:
+        return values
+
+    @override
+    def part1(self, parsed: str) -> int:
+        pass
+
+    @override
+    def part2(self, parsed: str) -> int:
+        {"pass" if not is_christmas else "return None"}
+""")
+        if not test_file.exists():
+            test_file.write_text('""\n')
+
         init_year_file.write_text(f'"""AoC problems for {year}."""\n')
         init_day_file.write_text(f'""""Day {day} problem for AoC {year}."""\n')
         in_file.write_text(data)
@@ -84,15 +111,25 @@ def run(
 
     ans1, ans2 = solve(day=day, year=year, data=puzzle_input)
 
-    out_file_a.write_text(ans1)
-
+    if ans1 is not None:
+        out_file_a.write_text(ans1)
     if ans2 is not None:
         out_file_b.write_text(ans2)
 
     rich.print(f"Part a: {ans1}\nPart b: {ans2}")
 
 
-def solve(*, year: int, day: int, data: str) -> tuple[str, str | None]:
+def coerce_solution(solution: str | int | None) -> None | str:
+    match solution:
+        case None:
+            return None
+        case str():
+            return solution
+        case int():
+            return str(solution)
+
+
+def solve(*, year: int, day: int, data: str) -> tuple[str | None, str | None]:
     mod = importlib.import_module(f"aoc.Y{year}.D{day:0>2}.main")
 
     solution = cast(BaseSolution[Any], mod.Solution())
@@ -101,7 +138,7 @@ def solve(*, year: int, day: int, data: str) -> tuple[str, str | None]:
     part1 = solution.part1(parsed)
     part2 = solution.part2(parsed)
 
-    return (str(part1), str(part2))
+    return (coerce_solution(part1), coerce_solution(part2))
 
 
 def main() -> None:
