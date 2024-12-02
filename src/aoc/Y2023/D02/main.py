a6c55182a9081e52cb6aea459426f32707495878
Eli <88557639+lishaduck@users.noreply.github.com>
2024-12-01 19:50:40 -0600

    wip: 2023/02

---
+++ src/aoc/Y2023/D02/main.py
@@ -0,0 +1,90 @@
+from collections.abc import Iterable
+from dataclasses import dataclass
+import enum
+from enum import Enum
+import functools
+import re
+from typing import override
+
+from aoc.base import AOCError, BaseSolution
+
+
+@dataclass
+class Handful:
+    red: int
+    green: int
+    blue: int
+
+
+class Color(Enum):
+    red = enum.auto()
+    green = enum.auto()
+    blue = enum.auto()
+
+
+red_regex = re.compile("\\d red")
+green_regex = re.compile("\\d green")
+blue_regex = re.compile("\\d blue")
+
+
+def color_of_cube_set(cube_set: str) -> tuple[Color, int]:
+    red_match = red_regex.match(cube_set)
+    green_match = green_regex.match(cube_set)
+    blue_match = blue_regex.match(cube_set)
+
+    if red_match is not None:
+        return (Color.red, red_match.start())
+
+    if green_match is not None:
+        return (Color.green, green_match.start())
+
+    if blue_match is not None:
+        return (Color.blue, blue_match.start())
+
+    raise AOCError
+
+
+def total_color(parsed_cube_set: Iterable[tuple[Color, int]], color: Color) -> int:
+    def reducer(acc: int, new: tuple[Color, int]) -> int:
+        return acc + new[1] if new[0] == color else acc
+
+    return functools.reduce(reducer, parsed_cube_set, 0)
+
+
+class Solution(BaseSolution[list[list[Handful]]]):
+    @override
+    def parse(self, values: str) -> list[list[Handful]]:
+        """Convert string into a list of rounds consisting of a list of handfuls."""
+
+        return [
+            ([
+                Handful(
+                    total_color(parsed_cube_set, Color.red),
+                    total_color(parsed_cube_set, Color.green),
+                    total_color(parsed_cube_set, Color.blue),
+                )
+                for parsed_cube_set in [
+                    [color_of_cube_set(cube_set) for cube_set in handful.split(",")]
+                    for handful in game.split(";")
+                    if not game.startswith("Game")
+                ]
+            ])
+            for game in values.splitlines()
+        ]
+
+    @override
+    def part1(self, parsed: list[list[Handful]]) -> int:
+        def has_enough_reducer(acc: bool, new: Handful) -> bool:
+            return acc and new.red > 12 and new.green > 13 and new.blue > 14
+
+        a = [functools.reduce(has_enough_reducer, game, True) for game in parsed]
+
+        total = 0
+        for i in range(len(a)):
+            total += i
+
+        return total
+
+    @override
+    def part2(self, parsed: list[list[Handful]]) -> int:
+        pass
---
+++ src/aoc/Y2023/D02/test_02.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+import pytest
+
+from aoc.Y2023.D02.main import Solution
+
+p: Path = Path(__file__).resolve().parent
+
+in_file: Path = p / "02.in"
+
+
+@pytest.mark.xfail
+class TestSolution:
+    tested = Solution()
+
+    @pytest.mark.parametrize(
+        ("example", "example_answer"),
+        [
+            (
+                """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""",
+                8,
+            ),
+        ],
+    )
+    def test_example1(self, example: str, example_answer: int) -> None:
+        answer = self.tested.run(example, part=1)
+        assert answer == example_answer
+
+    def test_example2(self) -> None:
+        example2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+        answer2 = 281
+
+        answer = self.tested.run(example2, part=2)
+
+        assert answer == answer2
+
+    def test_snapshot1(self) -> None:
+        out_file: Path = p / "01a.out"
+
+        input_content = in_file.read_text()
+        output_snapshot = out_file.read_text()
+
+        answer = self.tested.run(input_content, part=1)
+
+        assert str(answer) == output_snapshot
+
+    @pytest.mark.xfail
+    def test_snapshot2(self) -> None:
+        out_file: Path = p / "01b.out"
+
+        input_content = in_file.read_text()
+        output_snapshot = out_file.read_text()
+
+        answer = self.tested.run(input_content, part=2)
+
+        assert str(answer) == output_snapshot
