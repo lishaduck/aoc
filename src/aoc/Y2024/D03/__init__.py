a3e785a6540c624b9eb2803700f2999eb6380db6
Eli <88557639+lishaduck@users.noreply.github.com>
2024-12-03 09:31:55 -0600

    feat: do "Mull It Over"

---
+++ src/aoc/Y2024/D03/__init__.py
@@ -0,0 +1,1 @@
+""""Day 3 problem for AoC 2024."""
---
+++ src/aoc/Y2024/D03/test_03.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+import pytest
+
+from aoc.Y2024.D03.main import Solution
+
+p: Path = Path(__file__).resolve().parent
+
+in_file: Path = p / "03.in"
+
+
+class TestSolution:
+    tested = Solution()
+
+    @pytest.mark.parametrize(
+        ("example", "example_answer"),
+        [
+            (
+                """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))""",
+                161,
+            ),
+        ],
+    )
+    def test_example1(self, example: str, example_answer: int) -> None:
+        answer = self.tested.run(example, part=1)
+        assert answer == example_answer
+
+    @pytest.mark.parametrize(
+        ("example", "example_answer"),
+        [
+            (
+                """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))""",
+                48,
+            ),
+        ],
+    )
+    def test_example2(self, example: str, example_answer: int) -> None:
+        answer = self.tested.run(example, part=2)
+
+        assert answer == example_answer
+
+    @pytest.mark.xfail
+    def test_snapshot1(self) -> None:
+        out_file: Path = p / "03a.out"
+
+        input_content = in_file.read_text(encoding="utf-8")
+        output_snapshot = out_file.read_text(encoding="utf-8")
+
+        answer = self.tested.run(input_content, part=1)
+
+        assert str(answer) == output_snapshot
+
+    @pytest.mark.xfail
+    def test_snapshot2(self) -> None:
+        out_file: Path = p / "03b.out"
+
+        input_content = in_file.read_text(encoding="utf-8")
+        output_snapshot = out_file.read_text(encoding="utf-8")
+
+        answer = self.tested.run(input_content, part=2)
+
+        assert str(answer) == output_snapshot
