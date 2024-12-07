5dea14c0f614827dd58a0baa4a80565b5416f799
Eli <88557639+lishaduck@users.noreply.github.com>
2024-12-06 23:30:00 -0600

    chore: update tests for day 5

--- src/aoc/Y2024/D05/test_05.py
+++ src/aoc/Y2024/D05/test_05.py
@@ -52,12 +52,40 @@ class TestSolution:
         answer = self.tested.run(example, part=1)
         assert answer == example_answer
 
+    @pytest.mark.xfail
     @pytest.mark.parametrize(
         ("example", "example_answer"),
         [
             (
-                """""",
-                0,
+                """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47""",
+                143,
             ),
         ],
     )
@@ -66,7 +94,6 @@ class TestSolution:
 
         assert answer == example_answer
 
-    @pytest.mark.xfail
     def test_snapshot1(self) -> None:
         out_file: Path = p / "05a.out"
 
